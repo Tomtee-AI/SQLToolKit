63a8933699930f45efb470f5eb421232ed808898
Tomtee-AI <tethomasson@gmail.com>
2026-03-17 12:19:39 -0500

    Create Backup_Blob_Verify.sql

---
+++ Azure/AzureSQLDB/Backup_Blob_Verify.sql
@@ -0,0 +1,4 @@
+RESTORE HEADERONLY 
+FROM URL = 'https://mystorageaccount.blob.core.windows.net/mycontainer/YourDatabaseName_Full_20250317.bak'
+WITH CREDENTIAL = 'https://mystorageaccount.blob.core.windows.net/';
+GO
